774ec715d169e36efa07f5754352a7c762d9439b
hjortsater <erik.hjortsater@gmail.com>
2026-02-24 23:35:54 +0100

    Bug fix (??)

--- src/imports.py
+++ src/imports.py
@@ -4,6 +4,5 @@ from typing import Self, Any, Union, List, Tuple, Optional, TYPE_CHECKING
 from customdecorators import *
 import ctypes
 import os
-from __future__ import annotations # Fixes the "Matrix not defined" error
 from functools import wraps
 
